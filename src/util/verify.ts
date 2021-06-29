fa67725d3184fee64c57d100cca76bc6296c34c0
WMXPY <wm@mengw.io>
2021-06-29 10:28:28 -0500

    fix regex

--- src/util/verify.ts
+++ src/util/verify.ts
@@ -6,7 +6,7 @@
 
 import { NilUUID } from "./nil";
 
-export const verifyUUIDVersion1Regex: RegExp = /^[0-9a-f]{8}-[0-9a-f]{4}-1[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+export const verifyUUIDVersion1Regex: RegExp = /^[0-9a-f]{8}-[0-9a-f]{4}-1[0-9a-f]{3}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 
 export const verifyUUIDVersion1 = (uuid: string): boolean => {
 
