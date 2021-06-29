eeb97c00975e152bd660c6de8849dcb990a6559f
WMXPY <wm@mengw.io>
2021-06-28 19:00:11 -0500

    implement create uuid time components

---
+++ src/declare.ts
@@ -0,0 +1,6 @@
+/**
+ * @author WMXPY
+ * @namespace Lazy
+ * @description Declare
+ */
+
--- src/time.ts
+++ src/time.ts
@@ -8,10 +8,40 @@ export type UUIDTimeComponents = {
 
     readonly low: Uint8Array;
     readonly medium: Uint8Array;
-    readonly high: Uint8Array;
-    readonly version: Uint8Array;
+    readonly highAndVersion: Uint8Array;
 };
 
-export const createUUIDTimeComponents = () => {
+/* eslint-disable @typescript-eslint/no-magic-numbers, no-bitwise */
+export const createUUIDTimeComponents = (date: Date = new Date()): UUIDTimeComponents => {
 
+    const timeValue: number = date.getTime();
+    const nanoSeconds: bigint = BigInt(timeValue) * 10000n + 122192928000000000n;
+
+    const nanoLow: number = Number(nanoSeconds % 0x100000000n);
+    const timeLow: Uint8Array = new Uint8Array([
+        nanoLow >>> 24 & 0xff,
+        nanoLow >>> 16 & 0xff,
+        nanoLow >>> 8 & 0xff,
+        nanoLow >>> 0 & 0xff,
+    ]);
+
+    const nanoMedium: number = Math.floor(Number(nanoSeconds / 0x100000000n)) % 0x10000;
+    const timeMedium: Uint8Array = new Uint8Array([
+        nanoMedium >>> 8 & 0xff,
+        nanoMedium >>> 0 & 0xff,
+    ]);
+
+    const nanoHigh: number = Math.floor(Number(nanoSeconds / 0x1000000000000n)) % 0x10000;
+    const timeHighAndVersion: Uint8Array = new Uint8Array([
+        (nanoHigh >>> 8 & 0xf) | 0x10,
+        nanoHigh >>> 0 & 0xff,
+    ]);
+
+    return {
+
+        low: timeLow,
+        medium: timeMedium,
+        highAndVersion: timeHighAndVersion,
+    };
 };
+/* eslint-enable @typescript-eslint/no-magic-numbers, no-bitwise */
